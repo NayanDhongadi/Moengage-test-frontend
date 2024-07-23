43c67a79fe654a71e3d3791a8bb3e1402df71abc
Nayan <nayandhongadi26@gmail.com>
2024-07-23 17:40:09 +0530

    add web push

---
+++ public/serviceworker.js
@@ -0,0 +1,7 @@
+// service-worker.js
+
+// Import MoEngage service worker script
+importScripts('https://cdn.moengage.com/serviceworker_cdn.min.latest.js');
+
+// Initialize MoEngage service worker
+moengage.registerWorker(self, 'QPRMH0VQMWX3XZM7NU3VC8RD_DEBUG'); // Replace 'YOUR_APP_ID' with your actual MoEngage App ID
