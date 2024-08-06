ad6a91906a14ba173399353dd9e12c4d71b3c356
adityartp19 <adityartp19@gmail.com>
2024-08-06 20:09:11 +0530

    scope added 2

---
+++ serviceWork/serviceworker.js
@@ -0,0 +1,13 @@
+importScripts("https://cdn.moengage.com/release/dc_2/serviceworker_cdn.min.latest.js");
+
+// self.addEventListener('install', function(event) {
+//     console.log('Service Worker installed custom2.');
+//   });
+  
+//   self.addEventListener('activate', function(event) {
+//     console.log('Service Worker activated custom2.');
+//   });
+  
+//   self.addEventListener('fetch', function(event) {
+//     console.log('Fetching:', event.request.url);
+//   });
