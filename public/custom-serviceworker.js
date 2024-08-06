908444a4969af6511a6426c406087b8b621e2210
adityartp19 <adityartp19@gmail.com>
2024-08-06 19:27:15 +0530

    check

--- public/custom-serviceworker.js
+++ public/custom-serviceworker.js
@@ -1,11 +1,11 @@
 importScripts("https://cdn.moengage.com/release/dc_2/serviceworker_cdn.min.latest.js");
 
 self.addEventListener('install', function(event) {
-    console.log('Service Worker installed.');
+    console.log('Service Worker installed custom.');
   });
   
   self.addEventListener('activate', function(event) {
-    console.log('Service Worker activated.');
+    console.log('Service Worker activated custom.');
   });
   
   self.addEventListener('fetch', function(event) {
