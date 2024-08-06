5d9b74e8760c25829cacba27e5962014df689869
Nayan <nayandhongadi26@gmail.com>
2024-08-06 11:13:03 +0530

    changes service path

--- src/App.js
+++ src/App.js
@@ -9,13 +9,17 @@ import Services from '../src/Components/Services'
 
 function App() {
 
+
+
+
+
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      
+
       navigator.serviceWorker.register(`${process.env.PUBLIC_URL}/serviceworker.js`)
-      
+
         .then((registration) => {
-          console.log("process.env.PUBLIC_URL",process.env.PUBLIC_URL);
+          console.log("process.env.PUBLIC_URL", process.env.PUBLIC_URL);
           console.log('ServiceWorker registration successful with scope:', registration.scope);
         })
         .catch((error) => {
@@ -24,27 +28,27 @@ function App() {
     });
   }
   return (
-   <>
-<Router>
-      <div>
-        <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
-            <li><Link to="/services">Services</Link></li>
-          </ul>
-        </nav>
-        
-        <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/about" element={<About/>} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/services" element={<Services />} />
-        </Routes>
-      </div>
-    </Router>
-   </>
+    <>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/about">About</Link></li>
+              <li><Link to="/contact">Contact</Link></li>
+              <li><Link to="/services">Services</Link></li>
+            </ul>
+          </nav>
+
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/services" element={<Services />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
